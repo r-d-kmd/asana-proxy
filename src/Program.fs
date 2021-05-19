9132d7d086367911495d7c2b67be254067b880c9
Herover <leon.l.a.nielsen@gmail.com>
2021-05-19 08:37:01 +0200

    Add HTTP API

--- src/Program.fs
+++ src/Program.fs
@@ -1,16 +1,24 @@
 namespace AsanaProxy
 
 open System
-
+open System.Text.RegularExpressions
 module Program =
-    let from whom =
-        sprintf "from %s" whom
-
+    let (|Int|_|) str =
+       match System.Int32.TryParse(str : string) with
+       | (true, int) -> Some(int)
+       | _ -> None
+    let (|String|_|) str =
+       Some str
     [<EntryPoint>]
     let main argv =
-        (Asana.getWorkspaceTasks argv.[0] argv.[1])
-        |> Seq.map (fun task -> sprintf "%s - %s - %s - %s - %s\n" task.Title task.ProjectName task.SprintName task.ClosedDate task.SprintName)
-        //|> Seq.map (fun task -> sprintf "%A\n" task)
-        |> Seq.fold (fun acc e -> acc + e) ""
-        |> printf "%A"
+        match argv.[0] with
+        | Int i ->
+            Web.webServer argv
+        | String pat ->
+            (Asana.getWorkspaceTasks pat argv.[1])
+            |> Seq.map (fun task -> sprintf "%s - %s - %s - %s - %s\n" task.Title task.ProjectName task.SprintName task.ClosedDate task.SprintName)
+            //|> Seq.map (fun task -> sprintf "%A\n" task)
+            |> Seq.fold (fun acc e -> acc + e) ""
+            |> printf "%A"
+        | _ -> printfn ""
         0 // return an integer exit code
---
+++ src/web.fs
@@ -0,0 +1,55 @@
+namespace AsanaProxy
+
+open System
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+module Web =
+    let itemHandler (workspace : string) : HttpHandler =
+        fun (_ : HttpFunc) (ctx : HttpContext) ->
+            match ctx.TryGetQueryStringValue "pat" with
+            | Some pat -> ctx.WriteJsonAsync (Asana.getWorkspaceTasks pat workspace)
+            | None -> ctx.SetStatusCode 401
+                      ctx.WriteTextAsync "missing pat"
+
+    let endpoints =
+       choose [
+            route "/ping" >=> text "pong"
+            routef "/items/%s" itemHandler
+        ]
+
+    (* let notFoundHandler =
+        "Not Found"
+        |> text
+        |> RequestErrors.notFound *)
+
+    let configureApp (appBuilder : IApplicationBuilder) =
+        appBuilder
+            .UseRouting()
+            .UseGiraffe(endpoints)
+            //.UseGiraffe(notFoundHandler)
+
+    let configureServices (services : IServiceCollection) =
+        services
+            .AddRouting()
+            .AddGiraffe()
+        |> ignore
+
+    let [<Literal>] BaseAddress = "0.0.0.0:8085"
+
+    let webServer args =
+        WebHost
+            .CreateDefaultBuilder(args)
+            .UseKestrel()
+            .Configure(configureApp)
+            .ConfigureServices(configureServices)
+            .UseUrls(sprintf "http://%s" BaseAddress)
+            .Build()
+            .Run()
+
+
+
